504b1881347d5a11fee05d6872b3992627e175c6
Danial Kurtumerov <kur7umerovd@gmail.com>
2024-10-14 22:24:24 +0500

    feat: tier logic
    Added tier logic.

---
+++ src/logic/tier.py
@@ -0,0 +1,15 @@
+"""Tier logic module."""
+
+from enum import IntEnum
+
+
+class Tier(IntEnum):
+    """Tier enumerator."""
+
+    first: int = 1
+    second: int = 2
+    third: int = 3
+    fourth: int = 4
+    fifth: int = 5
+    sixth: int = 6
+    seventh: int = 7
